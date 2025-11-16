c764e60d3698999e055f556c9ea209d789c092a9
sirak <sirakotsegaye@gmail.com>
2025-11-16 05:07:14 -0800

    abrelo hd

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,14 @@
+# first declare a variable that recive input form user
+# create a for loop that loop from input num for 1 upto 10
+#   for index in range(10):
+#         mulitplication = number * index
+# then print mulitplication
+
+
+
 number = float(input("Enter a number to see its multiplication table: "))
 
 for index in range(1, 11):
     mulitplication = number * index
+    # print(f"{number} * {index} = {mulitplication}")
     print(mulitplication)
